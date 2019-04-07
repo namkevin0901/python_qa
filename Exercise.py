276477afbdd8d76c93721f089fa104d953178c75
HuynhKhanh <khanhhm.94@gmail.com>
2019-04-07 23:59:04 +0700

    format code

--- Exercise.py
+++ Exercise.py
@@ -1,20 +1,23 @@
 def show_star(n):
-    if n>0:
-        for x in range (1,n+1):
+    if n > 0:
+        for x in range(1, n+1):
             print(x*"*")
     else:
         print("bạn nhập số không đúng")
         input("hãy nhập lại số n")
 
-def showNumber(n):
+
+def show_number(n):
     for x in range(n):
-        if x%2 ==0:
-             print(x , "EVEN")
+        if x % 2 == 0:
+            print(x, "EVEN")
         else:
-            print(x , "ODD")
-if __name__ =='__main__':
+            print(x, "ODD")
+
+
+if __name__ == '__main__':
     n = int(input("hãy nhập số n:"))
     print("---------Bài 1 --------")
     show_star(n)
     print("---------Bài 2 --------")
-    showNumber(n)
+    show_number(n)
